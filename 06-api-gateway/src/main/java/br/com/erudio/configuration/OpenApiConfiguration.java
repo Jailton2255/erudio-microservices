7029f6c4c50aa02441ec26d0d2a501d24e729af0
Jailton Correia <jailton2255@gmail.com>
2022-11-19 20:15:47 -0300

    Ajustando as Configurações para Redenrizar o Swagger UI

--- 06-api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
+++ 06-api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -6,12 +6,16 @@ import java.util.List;
 import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class OpenApiConfiguration {
 	
-	public List<GroupedOpenApi> apis(
+	@Bean
+	@Lazy(false)
+	List<GroupedOpenApi> apis(
 			SwaggerUiConfigParameters config,
 			RouteDefinitionLocator locator) {
 		
@@ -19,7 +23,7 @@ public class OpenApiConfiguration {
 		
 		definitions.stream().filter(
 				routeDefinition -> routeDefinition.getId()
-				    .matches(".*-services"))
+				    .matches(".*-service"))
 				    	.forEach(routeDefinition -> {
 				    		String name = routeDefinition.getId();
 				    		config.addGroup(name);
